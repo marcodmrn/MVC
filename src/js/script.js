425c2cb24fb589b0d4223c412d0d8a376457d953
Marco <56915570+marcodmrn@users.noreply.github.com>
2020-05-26 14:40:11 +0200

    js-cookie

--- src/js/script.js
+++ src/js/script.js
@@ -4,44 +4,88 @@ document.addEventListener("DOMContentLoaded", function () {
       this.pages = [
         {
           title: "Homepage",
-          url: "/",
+          url: "#",
           background: "red",
         },
         {
           title: "Contact",
-          url: "/contact",
+          url: "#contact",
           background: "green",
+          form: true,
         },
       ];
     }
 
     getPageByUrl(url) {
-      return this.pages.find((page) => page.url === url);
+      return this.pages.find((page) => page.url == url);
     }
   }
 
   class View {
-    constructor(content) {
-      this.content = document.querySelector(`.${content}`);
+    constructor(pages) {
+      this.container = document.querySelector(".container");
+      this.container.innerHTML = "";
+      this.addHeader(pages);
     }
 
-    changeTitle(word) {
-      this.content.innerHTML = word;
+    addHeader(pages) {
+      console.log(pages);
+      const homeButton = document.createElement("button");
+      const contactButton = document.createElement("button");
+
+      homeButton.innerText = "Homepage";
+      contactButton.innerText = "Contact";
+
+      this.container.appendChild(homeButton);
+      this.container.appendChild(contactButton);
+
+      homeButton.addEventListener("click", () => {
+        location.hash = "#";
+      });
+
+      contactButton.addEventListener("click", () => {
+        location.hash = "#contact";
+      });
+    }
+
+    changeTitle(text) {
+      const title = document.createElement("h1");
+
+      title.innerText = text;
+
+      this.container.appendChild(title);
     }
 
     changeBackground(color) {
-      this.content.style.background = color;
+      this.container.style.background = color;
+    }
+
+    addForm() {
+      let input = document.createElement("input");
+      input.setAttribute("type", "text");
+      input.classList.add("input-text");
+
+      this.container.appendChild(input);
     }
   }
 
   function controller() {
-    const data = new Model();
-    const currentPage = data.getPageByUrl("/contact");
+    let data = new Model();
 
-    const page = new View("content");
+    const currentPage = data.getPageByUrl(location.hash || "#");
+
+    let page = new View(data.pages);
     page.changeTitle(currentPage.title);
     page.changeBackground(currentPage.background);
+
+    if (currentPage.form) {
+      page.addForm();
+    }
   }
+
+  window.addEventListener("hashchange", () => {
+    controller();
+  });
 
   controller();
 });
